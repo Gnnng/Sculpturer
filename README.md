36097fc17cd74b92e50ad0719136d40cf20d26ba
Gnnng <gnnnnng@gmail.com>
2015-06-18 09:05:20 +0800

    Add initial version and doc

---
+++ README.md
@@ -0,0 +1,7 @@
+# Development
+
+- OS: Mac OS X 10.10.3
+- Xcode: Version 6.1 (6A1052d)
+- GL: 2.1 INTEL-10.6.20
+
+
--- Sculpturer/main.cpp
+++ Sculpturer/main.cpp
@@ -7,9 +7,53 @@
 //
 
 #include <iostream>
+#include <string>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/GLUT.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+GLfloat bgColor[4] = {0.1, 0.1, 0.1, 0};
+
+bool init() {
+    glClearColor(bgColor[0], bgColor[1], bgColor[2], bgColor[3]);
+    glColor3f(0, 0, 0);
+    
+    return true;
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glShadeModel(GL_SMOOTH);
+    
+    glEnable(GL_DEPTH_BUFFER_BIT);
+    
+    glBegin(GL_TRIANGLES);
+    glColor3f(0, 1, 1);
+    glVertex2f(0, 0);
+    glVertex2f(0, 0.5);
+    glVertex2f(0.5, 0);
+    glEnd();
+    
+    glFlush();
+    
+    glutSwapBuffers();
+}
+
+int main(int argc, char * argv[]) {
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    
+    glutCreateWindow("A Demo");
+    
+    glutDisplayFunc(display);
+    
+    init();
+    
+    std::cout << "glut version is " << glGetString(GL_VERSION) << std::endl;
+    glutMainLoop();
     return 0;
 }
