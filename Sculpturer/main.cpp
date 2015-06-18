0a1dd0cf83372f95ed43ec8e8ba05a59fbde7141
Gnnng <gnnnnng@gmail.com>
2015-06-18 08:41:42 +0800

    Initial Commit

---
+++ Sculpturer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sculpturer
+//
+//  Created by Gong Deli on 6/18/15.
+//  Copyright (c) 2015 Gong Deli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
